2940811271f80410d366139f811524858639df2e
Sandy <sandyvasquez9@gmail.com>
2021-01-20 23:15:20 -0800

    Added MovieCard component for a movie

---
+++ src/components/MovieCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const MovieCard = ({
+  movie: {
+    adult,
+    backdrop_path: backdropPath,
+    genre_ids: genreIds,
+    id,
+    original_language: originalLanguage,
+    original_title: originalTitle,
+    overview,
+    popularity,
+    poster_path: posterPath,
+    release_date: releaseDate,
+    title,
+    video,
+    vote_average: voteAverage,
+    vote_count: voteCount,
+  },
+}) => (
+  <div className="row">
+    <div className="col s12">
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">{title}</span>
+          <p>{overview}</p>
+        </div>
+        <div className="card-action">
+          <button
+            className="btn waves-effect waves-light"
+            type="submit"
+            name="action"
+          >
+            Submit
+            <i className="material-icons right">send</i>
+          </button>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default MovieCard;
