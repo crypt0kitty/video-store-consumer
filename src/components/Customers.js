298c0aec43e7044c3be2c7366b8b00a106754fb2
Sandy <sandyvasquez9@gmail.com>
2021-01-20 23:12:41 -0800

    moved customers from App.js to Customers.js

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,15 +1,26 @@
-import React from 'react';
 import Customer from './Customer';
+import React, { useState, useEffect } from 'react';
 
-const Customers = (props) => {
-  const { customers = [] } = props;
+function Customers() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    /* data from: https://api.themoviedb.org/3/movie/550?api_key=fab6e945ca8bd693a69acea57b30c0b9 */
+    fetch('http://localhost:3000/customers')
+      // converting the data into json
+      .then((response) => response.json())
+      // received data as customers
+      .then((customers) => setCustomers(customers));
+  }, []);
+  // console.log(customers);
+
   return (
-    <div>
+    <div className="Customers">
       {customers.map((customer) => (
         <Customer key={customer.id} customer={customer} />
       ))}
     </div>
   );
-};
+}
 
 export default Customers;
