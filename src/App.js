99cb63ef2583574011248b87b09d33e6ac6f4ab5
Ayesha Asif <ayaseef@gmail.com>
2021-01-20 23:28:19 -0800

    Added react router to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { useState, useEffect } from 'react';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+/* what part of the code should be nested inside the router */
 import 'materialize-css/dist/css/materialize.min.css';
-
 import './App.css';
 import Customers from './components/Customers';
-
-function App() {
-  const [customers, setCustomers] = useState([]);
-
-  useEffect(() => {
-    /* fetch('https://api.themoviedb.org/3/movie/550?api_key=fab6e945ca8bd693a69acea57b30c0b9') */
-    fetch('http://localhost:3000/customers')
-      .then((res) => res.json())
-      .then((customers) => setCustomers(customers));
-  }, []);
-
-  return <div className="App">{<Customers customers={customers} />}</div>;
+import Navbar from './components/Navbar';
+import VideoSearch from './components/VideoSearch';
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/library">
+            <VideoLibrary />
+          </Route>
+          <Route path="/customers">
+            <h2>All Customers</h2>
+            <Customers />
+          </Route>
+          {/* <Route path="/search">
+            <VideoSearch />
+          </Route> */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+function VideoLibrary() {
+  return <h2>All Movies</h2>;
+}
+function MovieSearch() {
+  return <h2>Search</h2>;
+}
