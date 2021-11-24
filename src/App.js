8520284ded517b86567535a5fe1f6891a51df524
spv-hub <sandyvasquez9@gmail.com>
2021-11-23 17:34:24 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ import VideoSearch from './components/VideoSearch';
 import VideoLibrary from './components/VideoLibrary';
 import Banner from './movieBanner.png';
 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 export default function App() {
   return (
     <Router>
