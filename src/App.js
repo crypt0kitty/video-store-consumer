b17e8888b9a0b69ac2d5b874c466b8faa27164f7
spv-hub <sandyvasquez9@gmail.com>
2021-11-26 13:46:26 -0800

    adding port

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import Banner from './movieBanner.png';
 
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT || 3000, () => console.log(`Listening on ${ PORT }`))
 
 export default function App() {
   return (
