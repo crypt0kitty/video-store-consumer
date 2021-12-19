e67ac3a25bd44a170e9171f2b57c9a5c6f0dd9e6
Sandy Vasquez <sandyvasquez9@gmail.com>
2021-12-18 22:46:08 -0800

    removing prev install

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BiSidebar } from 'react-icons/bi';
 
 const Home = () => {
   return (
